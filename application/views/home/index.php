36b1eb930b1338a52003be65dd567e649b156ef0
Ganesh Bhosale <gdb.sci123@gmail.com>
2015-07-29 17:30:05 +0530

    Index File Change

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,1 +1,7 @@
-<h1>Hello, world!</h1>
+<center>
+	<br>
+	<br>
+	<br>
+	<h1><i class="fa fa-code"></i><br><br>CodeIngiter + BootStrap + Font Awesome</h1>
+	<h2><i class="fa fa-hand-peace-o"></i> Hello, world!</h2>
+</center>
